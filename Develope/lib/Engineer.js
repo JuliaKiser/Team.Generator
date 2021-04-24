475ce6b0d973dc42353fb61c7b68e129c2e147af
J_Kiser <jdkiser228@gmail.com>
2021-04-24 17:06:45 -0400

    adding engineer js info

--- Develope/lib/Engineer.js
+++ Develope/lib/Engineer.js
@@ -1,7 +1,20 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-    
+    constructor(name, id, email, role, gitHub){
+        //super gives access to parent constructor
+        super(name, id, email, role);
+        this.gitHub = gitHub;
+
+    }
+
+    getRole(){
+        return this.role;
+    }
+
+    getGithub(){
+        return this.gitHub;
+    }
 }
 
 module.exports = Engineer;
